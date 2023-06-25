17f206e846b4af16e6fb4e95c6d1371c01f4d27f
Jimmy morrens <jmorrens@prophets.be>
2023-06-25 21:23:41 +0200

    change output dir

--- src/tasks/javascript.ts
+++ src/tasks/javascript.ts
@@ -4,15 +4,16 @@ import { DEFAULT_OUTPUT_DIR } from '../constants'
 import postCssPlugin from '@deanc/esbuild-plugin-postcss'
 
 type compileJSProps = {
-    entryPoints: string[]
+    entryPoints: string[],
+    path: string,
 }
 
-export const compileJS = async ({entryPoints}: compileJSProps) => {
+export const compileJS = async ({entryPoints, path}: compileJSProps) => {
     console.log(`Compiling JS: ${entryPoints}`);
         await esbuild.build({
             entryPoints: entryPoints,
             bundle: true,
-            outdir: DEFAULT_OUTPUT_DIR,
+            outdir: `${DEFAULT_OUTPUT_DIR}/${path}`,
             plugins: [
                 postCssPlugin(),
             ],
--- src/utils/watch.ts
+++ src/utils/watch.ts
@@ -2,6 +2,7 @@ import chokidar, {FSWatcher} from 'chokidar'
 import path from 'path'
 import { compileJS } from '../tasks/javascript'
 import { REACT_EXT } from '../constants'
+import { DEFAULT_BANNER_DIR } from '../constants'
 
 export const watchInstance = (dir: string) => {
     return chokidar.watch(dir, {
@@ -16,11 +17,12 @@ export const watch = (watchInstance: FSWatcher, dir: string) => {
         const file = filename.split(path.sep)
         const s_filename = file[file.length - 1].split('.')
         const fileExt = s_filename[s_filename.length - 1]
+        const export_path = file.slice(0, -1).filter(name => name !== DEFAULT_BANNER_DIR).join(path.sep)
 
         if(typeof fileExt !== 'string') return
         
         if(REACT_EXT.includes(fileExt)){
-            await compileJS({entryPoints: [filename]})
+            await compileJS({entryPoints: [filename], path: export_path})
         }
     })
 }
