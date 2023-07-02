7e5d566d5edd2847e305def58618f4e9362ba1fc
Jimmy morrens <jmorrens@prophets.be>
2023-07-02 21:09:31 +0200

    generate HTML from react component

--- src/utils/reactToHtml.ts
+++ src/utils/reactToHtml.ts
@@ -1,14 +1,19 @@
+import fse from 'fs-extra';
 import {glob} from 'glob';
+import {renderToString, renderToStaticMarkup} from 'react-dom/server';
 
 const reactToHtml = async (dir: string) => {
-
-    const banner = await glob(dir + '/*.js', { ignore: 'node_modules/**', absolute: true });
-    console.log(banner);
+    const jsFile = `${dir}/index.js`;
+    const banner = await glob(jsFile, { ignore: 'node_modules/**', absolute: true });
 
     for (const file of banner) {
-        console.log(file);
         const component = await import(file);
-        console.log(component);
+        const html = `<!DOCTYPE html>${renderToStaticMarkup(component.default())}`;
+        fse.writeFile(`${dir}/index.html`, html, (err) => {
+            if (err) throw err;
+            fse.removeSync(jsFile);
+            console.log('The file has been saved!');
+        });
     }
 
     // const html = renderToString(component);
